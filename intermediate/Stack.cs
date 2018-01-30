bdb9d35236fc233f421c5d29c2b714498e44a220
CITS_DELL_05\Tang <Tang30738@gmail.com>
2018-01-30 11:23:28 +0700

    3rd commit

---
+++ intermediate/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intermediate
+{
+    class Stack
+    {
+        public void InsertStack()
+        {
+            var stack = new Stacks();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+
+        public class Stacks
+        {
+            public List<object> listStack = new List<object>();
+
+            public void Push(object obj)
+            {
+                if (obj == null) Console.WriteLine("Please enter a value !!");
+                listStack.Add(obj);
+            }
+
+            public object Pop()
+            {
+                var countList = listStack.Count();
+                if (countList == 0) return ("No data in Stack !!"); 
+                var pop = listStack[countList-1];
+                listStack.RemoveAt(countList-1);
+                return pop;
+            }
+
+            public void Clear()
+            {
+                listStack.Clear();
+            }
+
+        }
+
+        
+
+       
+    }
+}
---
+++ intermediate/Stopwatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intermediate
+{
+    class Stopwatch
+    {
+        public void stopwatch()
+        {
+            var t = 0;
+            var time = 0;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            while (t == 0)
+            {
+                Console.Write("Enter to start :");
+                var c = Console.ReadLine();
+                var b = (c == "reset") ? time = 0 : time = 1;
+                if (time == 1)
+                {
+                    watch.Start();
+                }
+                else
+                {
+                    watch = System.Diagnostics.Stopwatch.StartNew();
+                }
+
+                if (c != "reset")
+                {
+                    Console.Write("Enter to stop :");
+                    var s = Console.ReadLine();
+                    if (s == "stop") { t = 1; break; }
+                    watch.Stop();
+                    time = 1;
+                    TimeSpan ts = watch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                    Console.WriteLine("RunTime " + elapsedTime);
+                }
+                
+            }
+
+
+            //var elapsedMs = watch.ElapsedMilliseconds;
+        }
+    }
+}
